83f8949ea243054326c3e500d1717d9cdc16eb6b
Md Abdul Matin <matinpcc@yahoo.com>
2023-01-18 11:00:35 +0100

    new classes added

--- app.js
+++ app.js
@@ -1,47 +1,60 @@
+class Card {
+  suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+  names = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace",
+  ];
+  values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+  ];
+  constructor() {
+    this.suits;
+    this.names;
+    this.values;
+  }
+}
+
+const card = new Card();
+
 class Deck {
   deck = [];
-  constructor() {
-    const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
-    const names = [
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "Jack",
-      "Queen",
-      "King",
-      "Ace",
-    ];
-    const values = [
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "11",
-      "12",
-      "13",
-      "14",
-    ];
+  discardPile = [];
 
-    for (let i = 0; i < suits.length; i++) {
-      for (let j = 0; j < names.length; j++) {
-        const card = { Suit: suits[i], Name: names[j], Value: values[j] };
-        this.deck.push(card);
+  constructor() {
+    for (let i = 0; i < card.suits.length; i++) {
+      for (let j = 0; j < card.names.length; j++) {
+        const cardDeck = {
+          Suit: card.suits[i],
+          Name: card.names[j],
+          Value: card.values[j],
+        };
+        this.deck.push(cardDeck);
       }
     }
-    console.log(this.deck);
   }
-  // testing
   shuffleDeck() {
     for (let x = this.deck.length - 1; x > 0; x--) {
       let y = Math.floor(Math.random() * x);
@@ -50,60 +63,83 @@ class Deck {
       this.deck[y] = { ...deckCard };
     }
   }
+  dealCards(count) {
+    return this.deck.splice(0, count);
+  }
+
+  discard(cards) {
+    for (let i = 0; i < cards.length; i++) {
+      this.discardPile.push(cards[i]);
+    }
+  }
+  reset() {
+    let discardedCards = this.discardPile.splice(0, this.discardPile.length);
+    for (let i = 0; i < discardedCards.length; i++) {
+      this.deck.push(discardedCards[i]);
+    }
+  }
 }
 
-const deck2 = new Deck();
-deck2.shuffleDeck();
-console.log(deck2);
-// =============================
 class Player {
-  player1 = "Slim";
-  player2 = "Luke";
-
-  dealCards() {
-    let handPlayer1 = deck2.deck.splice(0, 5);
-    let handPlayer2 = deck2.deck.splice(5, 5);
-    let valueOfPlayer1Hand = handPlayer1.reduce((accumalator, card) => {
-      return accumalator + parseInt(card.Value);
-    }, 0);
-    let valueOfPlayer2Hand = handPlayer2.reduce((accumalator, card) => {
-      return accumalator + parseInt(card.Value);
-    }, 0);
-
-    console.log(handPlayer1, handPlayer2);
-    console.log(deck2);
-    console.log(`${this.player1}'s card value is ${valueOfPlayer1Hand}`);
-    console.log(`${this.player2}'s card value is ${valueOfPlayer2Hand}`);
-    //==========================
-    //discarding 2 cards by each player in discard deck
-    let discardCardsPile = handPlayer1.splice(3).concat(handPlayer2.splice(3));
-    console.log(handPlayer1, handPlayer2);
-    handPlayer1 = handPlayer1.concat(deck2.deck.splice(0, 2));
-    handPlayer2 = handPlayer2.concat(deck2.deck.splice(2, 2));
-    console.log(handPlayer1, handPlayer2);
-    //New value of player's card
-    valueOfPlayer1Hand = handPlayer1.reduce((accumalator, card) => {
-      return accumalator + parseInt(card.Value);
-    }, 0);
-    valueOfPlayer2Hand = handPlayer2.reduce((accumalator, card) => {
-      return accumalator + parseInt(card.Value);
-    }, 0);
-    console.log(`${this.player1}'s card value is ${valueOfPlayer1Hand}`);
-    console.log(`${this.player2}'s card value is ${valueOfPlayer2Hand}`);
-    //==============================
-    //Both players discard all cards to discard pile
-    let discardCardsAgain = handPlayer1.splice(0).concat(handPlayer2.splice(0));
-    let discardCardsPileAgain = discardCardsAgain.concat(discardCardsPile);
-    console.log(deck2);
-    // Return all cards to deck and empty discard pile
-    let deck3 = deck2.deck.concat(discardCardsPileAgain.splice(0));
-    console.log("Return all cards to deck and empty discard pile");
-    console.log(deck3);
+  name;
+  cards;
+  constructor(name) {
+    this.name = name;
+    this.cards = [];
   }
-  discardCards() {
-    console.log("Card is discarding");
+  addCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+      this.cards.push(cards[i]);
+    }
+  }
+  remove(count) {
+    return this.cards.splice(0, count);
   }
 }
-const players = new Player();
-players.dealCards();
-players.discardCards();
+
+class Main {
+  deck;
+  player1;
+  player2;
+  constructor() {
+    this.deck = new Deck();
+    this.player1 = new Player("Slim", this.deck);
+    this.player2 = new Player("Luke", this.deck);
+    this.deck.shuffleDeck();
+    console.log(this.deck);
+    this.player1.addCards(this.deck.dealCards(5));
+    console.log(this.player1);
+    this.deck.shuffleDeck();
+    console.log(this.deck);
+    this.player2.addCards(this.deck.dealCards(5));
+    console.log(this.player2);
+    console.log(this.deck);
+    this.deck.discard(this.player1.remove(2));
+    console.log(this.player1);
+    this.deck.discard(this.player2.remove(2));
+    console.log(this.player2);
+
+    this.deck.shuffleDeck();
+    console.log(this.deck);
+    this.player1.addCards(this.deck.dealCards(2));
+    console.log(this.player1);
+    this.deck.shuffleDeck();
+    console.log(this.deck);
+    this.player2.addCards(this.deck.dealCards(2));
+    console.log(this.player2);
+    this.deck.shuffleDeck();
+    console.log(this.deck);
+
+    this.deck.discard(this.player1.remove(5));
+    console.log(this.player1);
+    this.deck.discard(this.player2.remove(5));
+    console.log(this.player2);
+
+    console.log(this.deck);
+
+    this.deck.reset();
+
+    console.log(this.deck);
+  }
+}
+const main = new Main();
