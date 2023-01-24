039e1d03b95d6af98270c2cf43c20aeae63bffad
Md Abdul Matin <matinpcc@yahoo.com>
2023-01-24 10:54:17 +0600

    cards value printed

--- app.js
+++ app.js
@@ -92,6 +92,11 @@ class Player {
       this.cards.push(cards[i]);
     }
   }
+  getValueOfCards() {
+    this.cards.reduce((accumalator, card) => {
+      return accumalator + parseInt(card.Value);
+    }, 0);
+  }
   remove(count) {
     return this.cards.splice(0, count);
   }
@@ -105,40 +110,74 @@ class Main {
     this.deck = new Deck();
     this.player1 = new Player("Slim", this.deck);
     this.player2 = new Player("Luke", this.deck);
+
+    // shuffle deck with 52 cards
     this.deck.shuffleDeck();
     console.log(this.deck);
+
+    //Slim will get 5 cards
     this.player1.addCards(this.deck.dealCards(5));
     console.log(this.player1);
-    this.deck.shuffleDeck();
-    console.log(this.deck);
+
+    // Slim's cards value
+    let cardsValue = this.player1.cards.reduce((accumalator, card) => {
+      return accumalator + parseInt(card.Value);
+    }, 0);
+    console.log(`${this.player1.name}'s Card value is ${cardsValue}`);
+    // this.deck.shuffleDeck();
+    // console.log(this.deck);
+
+    //Luke will get 5 cards
     this.player2.addCards(this.deck.dealCards(5));
     console.log(this.player2);
+
+    // Luke's cards value
+    let cardsValues = this.player2.cards.reduce((accumalator, card) => {
+      return accumalator + parseInt(card.Value);
+    }, 0);
+    console.log(`${this.player2.name}'s Card value is ${cardsValues}`);
+    // Deck is having 42 cards now
     console.log(this.deck);
+
+    //Slim discard 2 cards to the discard pile
     this.deck.discard(this.player1.remove(2));
     console.log(this.player1);
+
+    //Luke discard 2 cards to the discard pile
     this.deck.discard(this.player2.remove(2));
     console.log(this.player2);
 
-    this.deck.shuffleDeck();
-    console.log(this.deck);
+    // this.deck.shuffleDeck();
+    // console.log(this.deck);
+    //Slim will take 2 cards
     this.player1.addCards(this.deck.dealCards(2));
     console.log(this.player1);
-    this.deck.shuffleDeck();
-    console.log(this.deck);
+
+    // Slim's cards value
+    // this.deck.shuffleDeck();
+    // console.log(this.deck);
+    //Luke will take 2 cards
     this.player2.addCards(this.deck.dealCards(2));
     console.log(this.player2);
+
+    // Luke's cards value
+
+    //Deck is having 38 cards and discard pile is having 4 cards now
     this.deck.shuffleDeck();
     console.log(this.deck);
 
-    this.deck.discard(this.player1.remove(5));
-    console.log(this.player1);
-    this.deck.discard(this.player2.remove(5));
-    console.log(this.player2);
+    //Slim and Luke discard 5 cards to the discard pile
+    // this.deck.discard(this.player1.remove(5));
+    // console.log(this.player1);
 
+    // this.deck.discard(this.player2.remove(5));
+    // console.log(this.player2);
+
+    //Deck is having 38 cards and discard pile is having 14 cards now
     console.log(this.deck);
 
+    //Deck will have 52 caeds and discard pile is empty
     this.deck.reset();
-
     console.log(this.deck);
   }
 }
---
+++ main.js
@@ -0,0 +1,10 @@
+// export function sum(a, b) {
+//     return a + b;
+//   }
+
+//   export function multiply(a, b) {
+//     return a * b;
+//   }12345678Import med named export// named import
+//   import {sum, multiply} from './another-file.js';
+
+//   console.log(sum(35, 65)); //
